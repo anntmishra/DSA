5077a73249ffc8195d49949bde76b8fdd30a8b7e
Anant Mishra <144559155+anntmishra@users.noreply.github.com>
2025-02-11 14:14:39 +0530

    update

---
+++ C++ /Loops/01.cpp
@@ -0,0 +1,28 @@
+/*
+1. while
+2. do while
+3.for
+4.for each 
+*/
+
+//Program to display numbers from 1 to n
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n,i=1;
+    cout<<"Enter number: ";
+    cin>>n;
+    while (i<=n)
+    {
+        cout<<i<<endl;
+        i++;
+    }
+    return 0;
+    
+}
+  
+    
+
+
+
